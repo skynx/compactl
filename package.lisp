bb756a9c87fb796ea73998c09c473ff0dce47f4e
Sky Hester <skyh@spie.org>
2019-10-13 17:19:15 -0700

    export symbols, adding test and example packages

--- package.lisp
+++ package.lisp
@@ -1,7 +1,24 @@
 ;;;; package.lisp
 
 (defpackage #:compactl
-  (:use #:cl))
+  (:use #:cl)
+  (:documentation "Lisp-native compact data structures")
+  (:export
+   ;; essential protocol functions
+   #:access
+   #:rank
+   #:select
+   ;; classes
+   #:wavelet-tree
+   ))
 
 (defpackage #:compactl.huffman
   (:use #:cl #:pqueue))
+
+(defpackage #:compactl.test
+  (:use #:cl #:compactl #:compactl.huffman)
+  (:documentation "Testing namespace for COMPACTL"))
+
+(defpackage #:compactl.ex
+  (:use #:cl #:compactl #:compactl.huffman)
+  (:documentation "Examples of using COMPACTL"))
